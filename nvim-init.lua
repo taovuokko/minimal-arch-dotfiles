a8de728cdd88cf71639f52e05c061eac0c0a3a36
Anon <anon@example.com>
2025-04-12 19:26:38 +0300

    Minimal arch setup

--- nvim-init.lua
+++ nvim-init.lua
@@ -1,1 +1,322 @@
-/home/tv/.config/nvim/init.lua
+-- super key on Space
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- 🛠️ Perusasetukset
+vim.opt.number = true                   -- Näytä rivinumerot
+vim.opt.expandtab = true                -- Käytä välilyöntejä tabien sijaan
+vim.opt.tabstop = 4                     -- Tabit = 4 välilyöntiä
+vim.opt.shiftwidth = 4                  -- Automaattinen sisennys = 4 välilyöntiä
+vim.opt.smartindent = true              -- Älykäs sisennys
+vim.opt.completeopt = { "menu", "menuone", "noselect" } -- Täydennysasetukset
+vim.opt.clipboard = "unnamedplus"
+
+-- 🛠️ Plugin manager: Lazy.nvim
+local lazypath = vim.fn.stdpath("config") .. "/lazy"
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  -- 🔹 LSP ja auto-täydennys
+  { "neovim/nvim-lspconfig" },   -- LSP-konfiguraatio
+  { "hrsh7th/nvim-cmp" },        -- Autocomplete
+  { "hrsh7th/cmp-nvim-lsp" },    -- LSP-integraatio
+
+  -- 🔹 Navigointi ja tiedostohaku
+  { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
+
+  -- 🔹 Autopairs
+  {
+    "windwp/nvim-autopairs",
+    config = function()
+      require("nvim-autopairs").setup{}
+    end,
+  },
+
+  -- 🔹 Treesitter
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = { "c", "lua" },
+        highlight = { enable = true },
+      })
+    end,
+  },
+
+  -- 🔹 Formatter (clang-format for C/C++)
+  {
+    "mhartington/formatter.nvim",
+    config = function()
+      require("formatter").setup({
+        filetype = {
+          c = {
+            function()
+              return {
+                exe = "clang-format",
+                args = { "--assume-filename", vim.api.nvim_buf_get_name(0) },
+                stdin = true,
+              }
+            end,
+          },
+          cpp = {
+            function()
+              return {
+                exe = "clang-format",
+                args = { "--assume-filename", vim.api.nvim_buf_get_name(0) },
+                stdin = true,
+              }
+            end,
+          },
+        },
+      })
+    end,
+  },
+
+  -- 🔹 Statusrivi (lualine)
+  { "nvim-lualine/lualine.nvim" },
+
+  -- 🔹 Teema: catppuccin
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.g.catppuccin_flavour = "mocha"
+      vim.cmd("colorscheme catppuccin")
+    end,
+  },
+
+  -- 🔹 Tiedostoselain (nvim-tree)
+  {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("nvim-tree").setup{}
+      vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { desc = "Toggle File Explorer" })
+    end,
+  },
+
+
+  -- 🔹 Toggleterm (parempi terminaali)
+  {
+    "akinsho/toggleterm.nvim",
+    config = function()
+      require("toggleterm").setup{
+        open_mapping = false,  -- käytetään vain leader-keymappeja
+        direction = "float",
+      }
+    end,
+  },
+
+  -- 🔹 Debuggeri: nvim-dap ja UI
+  {
+    "mfussenegger/nvim-dap",
+    config = function()
+      local dap = require("dap")
+      dap.adapters.c = {
+        type = 'executable',
+        command = 'lldb-vscode', -- tai "gdb", jos käytät GDB:tä
+        name = "lldb"
+      }
+      dap.configurations.c = {
+        {
+          name = "Launch",
+          type = "c",
+          request = "launch",
+          program = function()
+            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+          end,
+          cwd = '${workspaceFolder}',
+          stopOnEntry = true,
+          args = {},
+        },
+      }
+    end,
+  },
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
+    config = function()
+      require("dapui").setup()
+      local dap, dapui = require("dap"), require("dapui")
+      dap.listeners.after.event_initialized["dapui_config"] = function() dapui.open() end
+      dap.listeners.before.event_terminated["dapui_config"] = function() dapui.close() end
+      dap.listeners.before.event_exited["dapui_config"] = function() dapui.close() end
+    end,
+  },
+
+  -- 🔹 Virheiden katselu (Trouble)
+  {
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  opts = {
+    preset = "modern",
+    spec = {
+      { "<leader>x", group = "Diagnostics" },
+      { "<leader>t", group = "Terminal" },
+      { "<leader>d", group = "Dashboard" },
+      { "<leader>f", group = "Find" },
+      { "<leader>c", group = "Config/Tools" },
+      { "<leader>r", group = "Run/Compile" },
+      { "<leader>l", group = "LSP" },
+      { "<leader>p", group = "Projects" },
+      { "<leader>w", proxy = "<c-w>", group = "Window Move" },
+      { "<leader>db", desc = "Toggle Breakpoint" },
+      { "<leader>dr", desc = "Debug Continue" },
+      { "<leader>do", desc = "Step Over" },
+      { "<leader>di", desc = "Step Into" },
+      { "<leader>rr", desc = "Build & Run C" },
+    },
+  },
+},
+  -- 🔹 Projektituki
+  {
+    "ahmedkhalf/project.nvim",
+    config = function()
+      require("project_nvim").setup({})
+      require("telescope").load_extension("projects")
+    end,
+  },
+
+  -- 🔹 Startup-näkymä (alpha-nvim) ja NecroVim ASCII art
+  {
+    "goolord/alpha-nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      local alpha = require("alpha")
+      local dashboard = require("alpha.themes.dashboard")
+
+      -- ASCII-logo NecroVimille
+      dashboard.section.header.val = {
+        [[  _        _______  _______  _______  _______            _________ _______ ]],
+        [[ ( (    /|(  ____ \(  ____ \(  ____ )(  ___  )  |\     /|\__   __/(       )]],
+        [[ |  \  ( || (    \/| (    \/| (    )|| (   ) |  | )   ( |   ) (   | () () |]],
+        [[ |   \ | || (__    | |      | (____)|| |   | |  | |   | |   | |   | || || |]],
+        [[ | (\ \) ||  __)   | |      |     __)| |   | |  ( (   ) )   | |   | |(_)| |]],
+        [[ | | \   || (      | |      | (\ (   | |   | |   \ \_/ /    | |   | |   | |]],
+        [[ | )  \  || (____/\| (____/\| ) \ \__| (___) |    \   /  ___) (___| )   ( |]],
+        [[ |/    )_)(_______/(_______/|/   \__/(_______)     \_/   \_______/|/     \|]],
+      }
+
+      -- Asetetaan ASCII-logon väri punaiseksi
+      dashboard.section.header.opts = {
+        position = "center",
+        hl = "Keyword",
+      }
+
+      -- Dashboardin painikkeet
+      dashboard.section.buttons.val = {
+        dashboard.button("e", "📂  Open File Explorer", ":NvimTreeToggle<CR>"),
+        dashboard.button("f", "🔍  Find File", ":Telescope find_files<CR>"),
+        dashboard.button("r", "🕘  Recent Files", ":Telescope oldfiles<CR>"),
+        dashboard.button("c", "⚙️  Edit Config", ":e ~/.config/nvim/init.lua<CR>"),
+        dashboard.button("q", "🚪  Quit", ":q<CR>")
+      }
+
+      alpha.setup(dashboard.config)
+    end,
+  },
+})  -- <-- Tämä sulkee require("lazy").setup
+
+-- 🛠️ LSP-asetukset (clangd C-kielelle)
+-- Lataa lspconfig-moduuli
+local lspconfig = require('lspconfig')
+
+-- Aseta LSP-palvelin (esimerkiksi ccls tai clangd)
+lspconfig.ccls.setup({
+    init_options = {documentFormatting = true},
+    settings = {
+        rootMarkers = {".ccls", ".git/"},
+        languages = {
+            lua = {
+                formatCommand = "lua-format -i",
+                formatStdin = true
+            }
+        }
+    }
+})
+
+
+-- 📝 Inline virheet ja hover-docit
+vim.diagnostic.config({
+  virtual_text = true,
+  float = { border = "rounded" },
+})
+vim.keymap.set("n", "<leader>lh", vim.lsp.buf.hover, { desc = "LSP Hover" })
+
+-- 🛠️ Autocompletion-asetukset
+local cmp = require("cmp")
+cmp.setup({
+  mapping = {
+    ["<Tab>"] = cmp.mapping.select_next_item(),
+    ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+  },
+  sources = {
+    { name = "nvim_lsp" },
+  },
+})
+
+-- 🛠️ Parempi tiedostonavigointi (Telescope find_files)
+vim.keymap.set("n", "<leader>ff", ":Telescope find_files<CR>", { desc = "Find Files" })
+
+-----------------------------------------------------------
+-- Uudet leader-key -pohjaiset komennot (ideat 1, 2, 3 ja 6)
+-----------------------------------------------------------
+-- 1️⃣ Terminaali leader-kombinaatiot
+vim.keymap.set("n", "<leader>tt", ":ToggleTerm direction=float<CR>", { desc = "Toggle Float Terminal" })
+vim.keymap.set("n", "<leader>th", ":ToggleTerm direction=horizontal<CR>", { desc = "Toggle Horizontal Terminal" })
+vim.keymap.set("n", "<leader>tv", ":ToggleTerm direction=vertical<CR>", { desc = "Toggle Vertical Terminal" })
+
+-- 2️⃣ Dashboard mapping
+vim.keymap.set("n", "<leader>d", function()
+  print("=== Dashboard ===")
+  print("e: Explorer (NvimTree)")
+  print("t: Terminal (float)")
+  print("c: Edit config")
+  print("q: Quit")
+end, { desc = "Dashboard Info" })
+vim.keymap.set("n", "<leader>de", ":NvimTreeToggle<CR>", { desc = "Toggle File Explorer" })
+vim.keymap.set("n", "<leader>dt", ":ToggleTerm direction=float<CR>", { desc = "Toggle Float Terminal" })
+vim.keymap.set("n", "<leader>dc", ":e ~/.config/nvim/init.lua<CR>", { desc = "Edit Config" })
+vim.keymap.set("n", "<leader>dq", ":q<CR>", { desc = "Quit" })
+
+-- 3️⃣ Automatisoidaan yleisiä tehtäviä
+vim.keymap.set("n", "<leader>w", ":w<CR>", { desc = "Save File" })
+vim.keymap.set("n", "<leader>q", ":q<CR>", { desc = "Quit File" })
+vim.keymap.set("n", "<leader>x", ":x<CR>", { desc = "Save and Quit" })
+vim.keymap.set("n", "<leader>rf", ":source %<CR>", { desc = "Reload Current File" })
+vim.keymap.set("n", "<leader>R", ":edit!<CR>", { desc = "Reset Changes" })
+
+-- 6️⃣ Käyttömukavuuden lisäykset: Ikkunoiden välillä liikkuminen
+vim.keymap.set("n", "<leader>wh", "<C-w>h", { desc = "Window Left" })
+vim.keymap.set("n", "<leader>wj", "<C-w>j", { desc = "Window Down" })
+vim.keymap.set("n", "<leader>wk", "<C-w>k", { desc = "Window Up" })
+vim.keymap.set("n", "<leader>wl", "<C-w>l", { desc = "Window Right" })
+
+-----------------------------------------------------------
+-- Loput alkuperäiset keymapit ja asetukset
+-----------------------------------------------------------
+-- 🛠️ Statusrivi päälle
+require("lualine").setup()
+
+-- 🛠️ Koodin automaattinen siistiminen tallennettaessa
+vim.api.nvim_create_autocmd("BufWritePost", {
+  pattern = {"*.c", "*.h"},
+  callback = function() vim.cmd(":FormatWrite") end,
+})
+
+-- 🛠️ Shortcut C-koodin kääntämiseen ja ajamiseen (Build & Run)
+vim.keymap.set("n", "<leader>rr", ":w<CR>:split | terminal gcc % -o %:r && ./%:r<CR>", { desc = "Build & Run" })
+
+-- 🐞 DAP-debuggerin keymapit (leader-pohjaisesti)
+vim.keymap.set("n", "<leader>db", function() require("dap").toggle_breakpoint() end, { desc = "Toggle Breakpoint" })
+vim.keymap.set("n", "<leader>do", function() require("dap").step_over() end, { desc = "Step Over" })
+vim.keymap.set("n", "<leader>di", function() require("dap").step_into() end, { desc = "Step Into" })
+vim.keymap.set("n", "<leader>dr", function() require("dap").continue() end, { desc = "Debug Continue" })
+
+-- 🏗 Projektivalinta (Telescope Project)
+vim.keymap.set("n", "<leader>fp", ":Telescope projects<CR>", { desc = "Projects" })
+
--- zshrc
+++ zshrc
@@ -1,1 +1,163 @@
-/home/tv/.zshrc
+
+
+
+source /usr/share/zsh/plugins/zsh-autocomplete/zsh-autocomplete.plugin.zsh
+
+source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh  # Pacman-asennus
+
+
+# 🎨 Paremmat värit LS / exa -komennolle (tumma tausta)
+export LS_COLORS='di=1;34:ln=1;36:so=1;35:pi=1;33:ex=1;92:*.sh=1;93:*.out=1;91:*.c=1;94:*.py=1;95:*.txt=1;97:'
+
+# 🎨 Värikoodit
+autoload -U colors && colors
+autoload -Uz vcs_info
+
+# 🏎️ Nopeampi prompt
+setopt PROMPT_SUBST
+
+
+
+
+# 🚀 Perusaliasit (paremmat työkalut)
+alias cat='bat --paging=never'  # Parempi cat
+alias ls='exa --icons --group-directories-first'  # Parempi ls
+alias ll='exa -l --icons'  # Pitkä listaus
+alias la='exa -la --icons'  # Näytä myös piilotiedostot
+alias grep='grep --color=auto'  # Värillinen grep
+alias df='df -h'  # Ihmislukuisempi df
+alias apu='bat /home/tv/helppi.md'
+alias du='du -h'  # Ihmislukuisempi du
+alias kuva="kitten icat"
+alias update="sudo pacman -Sy --noconfirm --needed archlinux-keyring && paru -Su --noconfirm"
+
+bindkey "^[[1;5D" backward-word
+bindkey "^[[1;5C" forward-word
+
+
+
+
+# 🔥 Zoxide - nopeampi cd (käyttää fzf:ää)
+eval "$(zoxide init zsh)"
+alias cd='z'  # Käytä "z hakemisto" siirtymiseen
+
+# 📂 1️⃣ Fuzzy Finder hakemistovaihtaja (FD + FZF + CD)
+function fcd() {
+  local dir
+  dir=$(fd --type d . ~/ | fzf --height=20% --reverse --preview 'exa -T --color=always {}') && cd "$dir" || return
+}
+alias cdf='fcd'  # Helppo alias
+
+# 📜 2️⃣ Fuzzy Finder tiedostohaku ja avaus (FD + FZF + NVIM)
+function vf() {
+  local file
+  file=$(fd --type f | fzf --height=20% --reverse --preview "bat --color=always {}") && nvim "$file" || return
+}
+alias v='vf'  # Lyhyt alias
+
+# 🔄 3️⃣ Jo avattujen tiedostojen uudelleenavaus (trackataan ja avataan fzf:llä)
+export RECENT_FILES=~/.recent_files
+touch "$RECENT_FILES"
+
+function track_file() {
+  echo "$1" >> "$RECENT_FILES"
+  sort -u -o "$RECENT_FILES" "$RECENT_FILES"
+}
+alias track='track_file'  # Lisää tämä komentoihin, joilla avaat tiedostoja
+
+function recent() {
+  local file
+  file=$(cat "$RECENT_FILES" | fzf --height=20% --reverse --preview "bat --color=always {}") && nvim "$file" || return
+}
+alias r='recent'  # Käytä "r" avatakseen viimeksi käytettyjä tiedostoja
+
+# 🔍 4️⃣ Fuzzy Finder komennot + man-page preview
+function fman() {
+  local cmd
+  cmd=$(compgen -c | sort -u | fzf --preview 'man {}') && man "$cmd"
+}
+alias manf='fman'  # Fuzzy man-haku
+
+# 🏎️ 5️⃣ Supernopea Ctrl+R historiahaku
+export FZF_DEFAULT_OPTS="--height=40% --reverse --border"
+bindkey '^R' fzf-history-widget
+
+
+# 🎨 Mukautettu ZSH Syntax Highlighting -värit (paremmat tummalle taustalle)
+typeset -A ZSH_HIGHLIGHT_STYLES
+
+ZSH_HIGHLIGHT_STYLES[default]='fg=#E6E6E6'        # Perusväri (vaaleaa harmaata)
+ZSH_HIGHLIGHT_STYLES[unknown-token]='fg=#FF5555'  # Tuntematon komento (punainen)
+ZSH_HIGHLIGHT_STYLES[builtin]='fg=#FFA500,bold'   # Sisäänrakennetut komennot (oranssi, lihavoitu) 🟠
+ZSH_HIGHLIGHT_STYLES[alias]='fg=#50FA7B'          # Aliasit (vihreä)
+ZSH_HIGHLIGHT_STYLES[reserved-word]='fg=#BD93F9'  # Varatut sanat (lila)
+ZSH_HIGHLIGHT_STYLES[suffix-alias]='fg=#8BE9FD'   # Aliasien perään tulevat osat (sininen)
+ZSH_HIGHLIGHT_STYLES[precommand]='fg=#FFB86C,bold'  # Prekomennot, kuten "sudo" (oranssi, bold) 🟠🔥
+ZSH_HIGHLIGHT_STYLES[command]='fg=#50FA7B'        # Komennot (vihreä)
+ZSH_HIGHLIGHT_STYLES[command-substitution]='fg=#FF79C6'  # `$(komento)` (pinkki)
+ZSH_HIGHLIGHT_STYLES[globbing]='fg=#BD93F9'       # Jokerimerkit (*, ?) (lila)
+ZSH_HIGHLIGHT_STYLES[history-expansion]='fg=#FF79C6' # Historiaoperaattorit (!, ^) (pinkki)
+ZSH_HIGHLIGHT_STYLES[path]='fg=#8BE9FD'           # Polut ja tiedostonimet (vaaleansininen)
+ZSH_HIGHLIGHT_STYLES[autodirectory]='fg=#FFB86C'  # Automaattiset hakemistot (oranssi)
+ZSH_HIGHLIGHT_STYLES[quoted]='fg=#F1FA8C'         # Lainausmerkeissä olevat asiat (keltainen)
+ZSH_HIGHLIGHT_STYLES[comment]='fg=#6272A4'        # Kommentit (#) (harmaa)
+ZSH_HIGHLIGHT_STYLES[arg0]='fg=#50FA7B,bold'      # Ensimmäinen argumentti (vihreä, bold)
+ZSH_HIGHLIGHT_STYLES[option]='fg=#FFB86C'         # Vaihtoehdot (--help, -v) (oranssi)
+ZSH_HIGHLIGHT_STYLES[redirection]='fg=#FF79C6'    # Uudelleenohjaukset (>, <, |) (pinkki)
+
+
+# 🖥️ Promptin värit
+HOST_COLOR="%F{cyan}"       # Koneen nimi
+DIR_COLOR="%F{blue}"        # Hakemisto
+GIT_COLOR="%F{magenta}"     # Git-haara
+RESET_COLOR="%f"            # Resetoi värit
+PROMPT_SYMBOL="❯"           # Promptin symboli
+PROMPT_COLOR="%F{green}"    # Promptin väri
+
+# 🔥 GIT-haaran näyttäminen
+precmd() { vcs_info }
+zstyle ':vcs_info:*' formats " %F{yellow}[%b]%f"
+
+# 🌈 Promptin ulkoasu
+PROMPT='${HOST_COLOR}%n@%m%f ${DIR_COLOR}%~%f${vcs_info_msg_0_} ${PROMPT_COLOR}${PROMPT_SYMBOL} %f'
+
+# 🏎️ Lisää tilaa komentoja varten
+RPROMPT=''
+
+
+
+# 🔄 Päivitä historia ja jaa istuntojen välillä
+HISTSIZE=50000        # Säilytä 50 000 komentoa istunnossa
+SAVEHIST=50000        # Tallenna myös tiedostoon
+HISTFILE=~/.zsh_history  
+
+# 🔄 Estetään tuplakomennot historiassa
+setopt hist_ignore_all_dups   # Poista duplikaatit historiasta
+setopt hist_ignore_space      # Älä tallenna komentoa, joka alkaa välilyönnillä
+setopt hist_find_no_dups      # Älä näytä samaa komentoa useaan kertaan haussa
+setopt hist_reduce_blanks     # Poista turhat välilyönnit
+setopt share_history          # Jaa historia istuntojen välillä (toimii tmux/kittyssä)
+setopt inc_append_history     # Päivitä historia reaaliajassa ilman, että pitää sulkea istuntoa
+
+# ⬆️⬇️ Parempi nuolinäppäinten historiahaku
+#bindkey "^[[A" up-line-or-search
+#bindkey "^[[B" down-line-or-search
+
+
+bindkey '^[[A' fzf-history-widget
+bindkey '^[[B' down-line-or-search
+
+
+# Lataa fzf:n widgetit Zsh:lle
+if [[ -f /usr/share/fzf/key-bindings.zsh ]]; then
+    source /usr/share/fzf/key-bindings.zsh
+fi
+
+
+# 🔧 Lataa vain tarvittavat pluginit
+if [[ -f ~/.zsh_plugins.sh ]]; then
+    source ~/.zsh_plugins.sh
+fi
+
+export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
+export PATH="$HOME/.local/bin:$PATH"
